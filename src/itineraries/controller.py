762d92c4eb21c3bb50735fafb0d1ae4264582d99
Tuyishime Johnson <j.tuyishime4@alustudent.com>
2026-06-11 07:37:52 +0200

    use service inside itinerary controller

--- src/itineraries/controller.py
+++ src/itineraries/controller.py
@@ -2,28 +2,18 @@ from fastapi import APIRouter, HTTPException, Depends, status
 from sqlalchemy.orm import Session
 from typing import Annotated
 from ..database import get_db
-from ..entities.itinerary import Itinerary
 from .model import ItineraryCreateRequest
+from . import service
 
 router = APIRouter(prefix="/itineraries", tags=["itineraries"])
-
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def create_itinerary(db: db_dependency, itinerary: ItineraryCreateRequest):
-    new_itinerary = Itinerary(trip_id=itinerary.trip_id, days=itinerary.days)
-    db.add(new_itinerary)
-    db.commit()
-    db.refresh(new_itinerary)
-    return new_itinerary
+async def create_itinerary(itinerary: ItineraryCreateRequest, db: db_dependency):
+    return service.create_itinerary(db, itinerary)
 
 
 @router.get("/{trip_id}", status_code=status.HTTP_200_OK)
-async def get_itinerary_by_trip_id(db: db_dependency, trip_id: int):
-    itinerary = db.query(Itinerary).filter(Itinerary.trip_id == trip_id).first()
-    if not itinerary:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Itinerary not found"
-        )
-    return itinerary
+async def get_itinerary_by_trip_id(trip_id: int, db: db_dependency):
+    return service.get_itinerary_by_trip_id(db, trip_id)
---
+++ src/itineraries/service.py
@@ -0,0 +1,24 @@
+from fastapi import HTTPException, status
+from sqlalchemy.orm import Session
+
+from .model import ItineraryCreateRequest
+from ..database import get_db
+from ..entities.itinerary import Itinerary
+from .model import ItineraryCreateRequest
+
+
+def create_itinerary(itinerary: ItineraryCreateRequest, db: Session):
+    new_itinerary = Itinerary(trip_id=itinerary.trip_id, days=itinerary.days)
+    db.add(new_itinerary)
+    db.commit()
+    db.refresh(new_itinerary)
+    return new_itinerary
+
+
+def get_itinerary_by_trip_id(trip_id: int, db: Session):
+    itinerary = db.query(Itinerary).filter(Itinerary.trip_id == trip_id).first()
+    if not itinerary:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Itinerary not found"
+        )
+    return itinerary
