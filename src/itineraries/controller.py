59483cd55d00af0b20011c10e295e667a7329cb1
Tuyishime Johnson <j.tuyishime4@alustudent.com>
2026-06-11 07:38:25 +0200

    use service inside itinerary controller

--- src/itineraries/controller.py
+++ src/itineraries/controller.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Depends, status
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from typing import Annotated
 from ..database import get_db
@@ -11,9 +11,9 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_itinerary(itinerary: ItineraryCreateRequest, db: db_dependency):
-    return service.create_itinerary(db, itinerary)
+    return service.create_itinerary(itinerary, db)
 
 
 @router.get("/{trip_id}", status_code=status.HTTP_200_OK)
 async def get_itinerary_by_trip_id(trip_id: int, db: db_dependency):
-    return service.get_itinerary_by_trip_id(db, trip_id)
+    return service.get_itinerary_by_trip_id(trip_id, db)
